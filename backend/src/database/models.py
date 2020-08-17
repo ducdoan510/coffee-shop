ff904a8277c5806ed9b7456cfaa040693ddeae15
duc.doan <3dmailboxes@gmail.com>
2020-08-18 00:01:11 +0800

    chore: Clean up redundant print statement

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -56,7 +56,6 @@ class Drink(db.Model):
     '''
 
     def short(self):
-        print(json.loads(self.recipe))
         short_recipe = [{'color': r['color'], 'parts': r['parts']} for r in json.loads(self.recipe)]
         return {
             'id': self.id,
